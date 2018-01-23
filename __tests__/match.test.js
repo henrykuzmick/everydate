410456d60f21d4bfb41dfdaa8690b72c31ee18e3
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2018-01-23 22:35:44 +0100

    Add matching

---
+++ __tests__/match.test.js
@@ -0,0 +1,128 @@
+import Everydate from '../src';
+
+describe('matching days', () => {
+  it('should return true for matching days', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [5],
+      measure: 'days'
+    });
+    expect(recur.match('2018-02-15')).toEqual(true);
+    expect(recur.match('2018-02-10')).toEqual(true);
+  });
+  it('should return false for non matching days', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [5],
+      measure: 'days'
+    });
+    expect(recur.match('2018-02-16')).toEqual(false);
+  });
+});
+
+describe('matching weeks', () => {
+  it('should return true for matching weeks', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'weeks'
+    });
+    expect(recur.match('2018-02-24')).toEqual(true);
+    expect(recur.match('2018-02-10')).toEqual(true);
+  });
+  it('should return false for non matching weeks', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'weeks'
+    });
+    expect(recur.match('2018-02-17')).toEqual(false);
+  });
+});
+
+describe('matching months', () => {
+  it('should return true for matching months', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'months'
+    });
+    expect(recur.match('2018-02-10')).toEqual(true);
+    expect(recur.match('2018-04-10')).toEqual(true);
+    expect(recur.match('2018-12-10')).toEqual(true);
+  });
+  it('should return false for non matching months', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'months'
+    });
+    expect(recur.match('2018-03-10')).toEqual(false);
+    expect(recur.match('2018-04-11')).toEqual(false);
+  });
+});
+
+describe('matching years', () => {
+  it('should return true for matching years', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'years'
+    });
+    expect(recur.match('2018-02-10')).toEqual(true);
+    expect(recur.match('2020-02-10')).toEqual(true);
+    expect(recur.match('2022-02-10')).toEqual(true);
+  });
+  it('should return false for non matching years', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'years'
+    });
+    expect(recur.match('2020-02-11')).toEqual(false);
+    expect(recur.match('2019-02-10')).toEqual(false);
+  });
+});
+
+describe('matching daysOfWeek', () => {
+  it('should return true for matching daysOfWeek', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'daysOfWeek'
+    });
+    expect(recur.match('2018-02-13')).toEqual(true);
+    expect(recur.match('2018-11-13')).toEqual(true);
+  });
+  it('should return false for non matching daysOfWeek', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [2],
+      measure: 'daysOfWeek'
+    });
+    expect(recur.match('2018-02-10')).toEqual(false);
+    expect(recur.match('2018-02-06')).toEqual(false);
+    expect(recur.match('2018-02-14')).toEqual(false);
+  });
+});
+
+describe('matching daysOfMonth', () => {
+  it('should return true for matching daysOfMonth', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [15],
+      measure: 'daysOfMonth'
+    });
+    expect(recur.match('2018-02-15')).toEqual(true);
+    expect(recur.match('2028-11-15')).toEqual(true);
+  });
+  it('should return false for non matching daysOfMonth', () => {
+    const recur = new Everydate({
+      start: '2018-02-10',
+      units: [15],
+      measure: 'daysOfMonth'
+    });
+    expect(recur.match('2018-02-10')).toEqual(false);
+    expect(recur.match('2018-03-14')).toEqual(false);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@
 import addDays from 'date-fns/add_days';
 import subDays from 'date-fns/sub_days';
 import addWeeks from 'date-fns/add_weeks';
-import subMonths from 'date-fns/sub_months';
 import addMonths from 'date-fns/add_months';
 import addYears from 'date-fns/add_years';
 import compareAsc from 'date-fns/compare_asc';
 import format from 'date-fns/format';
 import getDay from 'date-fns/get_day';
 import getDate from 'date-fns/get_date';
+import getMonth from 'date-fns/get_month';
+import isBefore from 'date-fns/is_before';
+import isAfter from 'date-fns/is_after';
+import differenceInDays from 'date-fns/difference_in_days';
+import differenceInMonths from 'date-fns/difference_in_months';
+import differenceInYears from 'date-fns/difference_in_years';
+import isSameDay from 'date-fns/is_same_day';
 
 type MeasureType =
   | 'days'
@@ -37,7 +43,8 @@ class EveryDate {
     this.measure = measure;
   }
 
-  next(times: number) {
+  next(times: number, options: Object = {}) {
+    // const startDate = options.startDate ? new Date(options.startDate) : this.start;
     const res = [];
     for (let i = 0; i <= times; i++) {
       for (let j = 0; j < this.units.length; j++) {
@@ -91,6 +98,70 @@ class EveryDate {
     return Array.from(
       new Set(res.map(date => format(date, 'YYYY-MM-DD')))
     ).slice(0, times);
+  }
+
+  match(date: string) {
+    const testDate = new Date(date);
+    if (isBefore(testDate, this.start)) {
+      return false;
+    }
+
+    const diffInDays = differenceInDays(this.start, testDate);
+    const diffInMonths = differenceInMonths(this.start, testDate);
+    const diffInYears = differenceInYears(this.start, testDate);
+
+    for (let i = 0; i < this.units.length; i++) {
+      const unit = this.units[i];
+      switch (this.measure) {
+        case 'days':
+          if (diffInDays % unit === 0) {
+            return true;
+          }
+          break;
+        case 'weeks':
+          if ((diffInDays % unit) * 7 === 0) {
+            return true;
+          }
+          break;
+        case 'months':
+          if (diffInMonths % unit === 0) {
+            if (getDate(this.start) === getDate(testDate)) {
+              return true;
+            }
+          }
+          break;
+        case 'years':
+          if (diffInYears % unit === 0) {
+            if (getMonth(this.start) === getMonth(testDate)) {
+              if (getDate(this.start) === getDate(testDate)) {
+                return true;
+              }
+            }
+          }
+          break;
+        case 'daysOfWeek':
+          if (unit === getDay(testDate)) {
+            return true;
+          }
+          break;
+        case 'daysOfMonth':
+          if (unit === getDate(testDate)) {
+            return true;
+          }
+          break;
+      }
+    }
+
+    // let testDay = this.start;
+    // switch(this.measure) {
+    //   case 'days': {
+    //     while(isBefore(testDay, testDate)) {
+    //       for (let j = 0; j < this.units.length; j++) {
+    //       }
+    //     }
+    //   }
+    // }
+    return false;
   }
 }
 
